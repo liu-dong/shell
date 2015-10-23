6e9a50e173fd2cf02b49a41b5cd1f5e3571d128a
yuanquan.ld <yuanquan.ld@taobao.com>
2015-10-23 14:29:10 +0800

    modify desc

--- awk/git_code_line_num.sh
+++ awk/git_code_line_num.sh
@@ -3,6 +3,6 @@
 # write by    : yuanquan.ld@gmail.com
 # date        : 2015-10-11
 # version     : 0.0.1
-# desc        : 统计所有commit，insert和delete的行数
+# desc        : Statistics all the commit, insert and delete rows
 
 git log --shortstat | grep 'changed' | sed 's/.*changed,//' | sed -E 's/insertions?//' | sed -E 's/deletions?//' | sed 's/(//g' | sed 's/)//g' | awk -F ',' '{if(NF==2) print $1"\n"$2; else print $1;}' | sed 's/^ //' | awk '{if($2=="+") add=add+$1; else subs=subs+$1}END{printf " added lines: %s \n removed lines : %s \n total lines: %s\n",add,subs,add-subs}'
